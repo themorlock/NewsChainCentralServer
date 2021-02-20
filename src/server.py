219fe70d8f15f7dcb899e413eff106c6bd91cb95
Saaketh Vangati <saaketh.vangati@gmail.com>
2021-02-20 08:32:14 -0600

    Slight revisions

--- src/server.py
+++ src/server.py
@@ -8,8 +8,9 @@ peer_addresses = set()
 @app.route('/get_peer_addresses', methods=['GET'])
 def home():
     return_peer_addresses = list(peer_addresses).copy()
+    return_peer_addresses.remove(flask.request.remote_addr)
     peer_addresses.add(flask.request.remote_addr)
-    return flask.jsonify(return_peer_addresses[:int(flask.request.args.get("n"))])
+    return flask.jsonify(return_peer_addresses)
 
 
 if __name__ == '__main__':
